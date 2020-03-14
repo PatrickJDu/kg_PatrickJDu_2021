68782987635a2c647ef1b36053fb5eac2e107ffe
Patrick Du <dupatrick@gmail.com>
2020-03-14 15:59:15 -0400

    Replacing dict for set to make code flow better with less if else statements

--- main.py
+++ main.py
@@ -2,55 +2,46 @@ import sys
 
 
 class Map:
-    map_char = None
+    map_set = None
 
     def __init__(self):
-        self.map_char = {}
-
-    """
-    One to One does not exist if the length of the strings are different. In order for one to one to
-    """
+        self.map_set = set()
 
     @staticmethod
     def initial_check(str1, str2):
+        """
+        One to One does not exist if the length of the strings are different. In order for one to one to
+        :param str1: Initial string 1 passed from command line
+        :param str2: Initial string 2 passed from command line
+        :return: True or False
+        """
         if len(str1) < len(str2) or len(str2) < len(str1):
             return False
         return True
 
-    """
-    This is the brute force method that compares the character of the first to see if there are unique characters.
-    We only care about the uniqueness of the first string. O(n^2) time complexity O(1) space complexity
-    """
-
-    @staticmethod
-    def brute_force(str1):
-        for pos, value in enumerate(str1):
-            for second_pos in range(pos + 1, len(str1)):
-                if value == str1[second_pos]:
-                    return False
-        return True
-
-    """
-    This uses a dictionary to map each character in the string to another character in another string.
-    O(n) time complexity O(1) space complexity
-    """
-
-    def create_map(self, str1, str2):
-        for pos, value in enumerate(str1):
-            if value not in self.map_char:
-                self.map_char[value] = str2[pos]
-            else:
-                return False
-        return True
-
-    """
-    Start the one to one comparison
-    """
+    def create_set(self, str1, str2):
+        """
+        This replaces the need for if/else statements like the dictionary to store. Again we only care about the
+        uniqueness of string1. For example if string1 = foo and string2 = bar it would false because foo has 2 o's.
+        A set would catch if string1 has multiple of the same characters
+        O(n) time complexity and O(n) space complexity
+        :param str1: Initial string 1 passed from command line
+        :param str2 Initial string 2 passed from command line
+        :return: True or False
+        """
+        self.map_set = {char for char in str1}
+        return True if len(self.map_set) == len(str2) else False
 
     def run(self, str1, str2):
+        """
+        Start the one to one comparison
+        :param str1: Initial string 1 passed from command line
+        :param str2: Initial string 2 passed from command line
+        :return: String version of True or False
+        """
         if not self.initial_check(str1, str2):
             return "false"
-        if not self.create_map(str1, str2):
+        if not self.create_set(str1, str2):
             return "false"
         return "true"
 
